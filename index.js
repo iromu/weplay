75354436368c13f299e4a5474e3dc78923a7ac2a
Iván Rodríguez Murillo <wantez@gmail.com>
2016-12-04 18:54:24 +0100

    ES6 support

--- index.js
+++ index.js
@@ -1,25 +1,24 @@
 'use strict';
 
 var sio = require('socket.io');
-
 var forwarded = require('forwarded-for');
 
 process.title = 'weplay-io';
 
-var port = process.env.WEPLAY_PORT || 3001;
-var io = module.exports = sio(port);
-console.log('listening on *:' + port);
+const port = process.env.WEPLAY_PORT || 3001;
+const io = module.exports = sio(port);
+console.log(`listening on *:${port}`);
 
-var throttle = process.env.WEPLAY_IP_THROTTLE || 100;
+const throttle = process.env.WEPLAY_IP_THROTTLE || 100;
 
 // redis socket.io adapter
-var uri = process.env.WEPLAY_REDIS || 'redis://localhost:6379';
+const uri = process.env.WEPLAY_REDIS || 'redis://localhost:6379';
 io.adapter(require('socket.io-redis')(uri));
 
 // redis queries instance
-var redis = require('./redis')();
+const redis = require('./redis')();
 
-var keys = {
+const keys = {
     right: 0,
     left: 1,
     up: 2,
@@ -30,29 +29,29 @@ var keys = {
     start: 7
 };
 
-var uid = process.env.WEPLAY_SERVER_UID || port;
+const uid = process.env.WEPLAY_SERVER_UID || port;
 console.log('server uid %s', uid);
 
 io.total = 0;
-io.on('connection', function (socket) {
-    var req = socket.request;
-    var ip = forwarded(req, req.headers);
+io.on('connection', socket => {
+    const req = socket.request;
+    const ip = forwarded(req, req.headers);
     console.log('client ip %s', JSON.stringify(ip));
 
     // keep track of connected clients
     updateCount(++io.total);
-    socket.on('disconnect', function () {
+    socket.on('disconnect', () => {
         updateCount(--io.total);
     });
 
     // send events log so far
-    redis.lrange('weplay:log', 0, 20, function (err, log) {
+    redis.lrange('weplay:log', 0, 20, (err, log) => {
         if (!Array.isArray(log)) return;
-        log.reverse().forEach(function (data) {
+        log.reverse().forEach(data => {
             data = data.toString();
-            var args = JSON.parse(data);
+            const args = JSON.parse(data);
             if (Array.isArray(args)) {
-                socket.emit.apply(socket, args);
+                socket.emit(...args);
             } else {
                 console.error(data);
             }
@@ -60,16 +59,16 @@ io.on('connection', function (socket) {
     });
 
     // broadcast moves, throttling them first
-    socket.on('move', function (key) {
+    socket.on('move', key => {
         if (null == keys[key]) return;
-        redis.get('weplay:move-last:' + ip, function (err, last) {
+        redis.get(`weplay:move-last:${ip}`, (err, last) => {
             if (last) {
                 last = last.toString();
                 if (Date.now() - last < throttle) {
                     return;
                 }
             }
-            redis.set('weplay:move-last:' + ip, Date.now());
+            redis.set(`weplay:move-last:${ip}`, Date.now());
             redis.publish('weplay:move', keys[key]);
             socket.emit('move', key, socket.nick);
             broadcast(socket, 'move', key, socket.nick);
@@ -77,12 +76,12 @@ io.on('connection', function (socket) {
     });
 
     // send chat mesages
-    socket.on('message', function (msg) {
+    socket.on('message', msg => {
         broadcast(socket, 'message', msg, socket.nick);
     });
 
     // broadcast user joining
-    socket.on('join', function (nick) {
+    socket.on('join', nick => {
         if (socket.nick) return;
         socket.nick = nick;
         socket.emit('joined');
@@ -99,7 +98,7 @@ function updateCount(total) {
 // broadcast events and persist them to redis
 
 function broadcast(socket/*, …*/) {
-    var args = Array.prototype.slice.call(arguments, 1);
+    const args = Array.prototype.slice.call(arguments, 1);
     redis.lpush('weplay:log', JSON.stringify(args));
     redis.ltrim('weplay:log', 0, 20);
     socket.broadcast.emit.apply(socket, args);
